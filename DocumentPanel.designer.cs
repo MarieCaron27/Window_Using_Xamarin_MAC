c4aafe0f139c1e23091fe351719a9387fbc0f7f1
marie <mariecaron27@gmail.com>
2026-07-03 12:53:26 +0200

    Document panel added

---
+++ DocumentPanel.designer.cs
@@ -0,0 +1,27 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Rider IDE
+//   to store outlets and actions made in Xcode.
+// If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace window
+{
+	partial class DocumentPanel
+	{
+		[Outlet]
+		AppKit.NSPanel DocumentPanelController { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (DocumentPanelController != null) {
+				DocumentPanelController.Dispose ();
+				DocumentPanelController = null;
+			}
+
+		}
+	}
+}
